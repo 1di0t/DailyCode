4c34be5670627682987b7e9252fb46e9102c636d
1di0t <jeonjerry0709@gmail.com>
2025-03-17 10:45:31 +0900

    3003_v1

--- test.py
+++ test.py
@@ -1,21 +1,9 @@
 import sys
 
+chess = list(map(int,sys.stdin.readline().rstrip().split(" ")))
+chess_gudie = [1,1,2,2,2,8]
 
-participants = int(sys.stdin.readline().rstrip())
-sizes = list(map(int,sys.stdin.readline().rstrip().split(" ")))
-bundle_T, bundle_P = map(int,sys.stdin.readline().rstrip().split(" "))
-t_order = 0
+for i in range(len(chess)):
+    chess_gudie[i] = chess_gudie[i] - chess[i]
 
-for i in range(len(sizes)):
-    if sizes[i] == 0:
-        continue
-    elif sizes[i] <= bundle_T:
-        t_order += 1
-    else:
-        t_order += sizes[i]//bundle_T
-        if sizes[i]%bundle_T != 0:
-            t_order += 1
-
-
-print(t_order)
-print(f"{participants//bundle_P} {participants%bundle_P}")
+print(" ".join(map(str,chess_gudie)))
