e92e966de71dfd358d39727ca7795c41fedff295
1di0t <jeonjerry0709@gmail.com>
2025-03-17 10:57:37 +0900

    21300_v1

--- test.py
+++ test.py
@@ -1,6 +1,5 @@
 import sys
 
-price = int(sys.stdin.readline().rstrip())
+bottle = list(map(int,sys.stdin.readline().rstrip().split(" ")))
 
-print(price*0.78)
-print(price*0.2*0.78+price*0.8)
+print(sum(bottle)*5)
