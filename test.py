3d8fc4892a083d667d4f0bd5287396eaa130077a
1di0t <jeonjerry0709@gmail.com>
2025-03-17 11:05:17 +0900

    30087_v1

--- test.py
+++ test.py
@@ -1,5 +1,16 @@
 import sys
 
-bottle = list(map(int,sys.stdin.readline().rstrip().split(" ")))
+semina = []
+semina_num = int(sys.stdin.readline().rstrip())
+room_num = [["Algorithm","204"],["DataAnalysis","207"],["ArtificialIntelligence","302"],["CyberSecurity","B101"]
+            ,["Network","303"],["Startup","501"],["TestStrategy","105"]]
 
-print(sum(bottle)*5)
+for i in range(semina_num):
+    semina.append(sys.stdin.readline().rstrip())
+
+for i in range(semina_num):
+    for j in range(len(room_num)):
+        if semina[i] == room_num[j][0]:
+            print(room_num[j][1])
+            break
+    
