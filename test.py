388d4005b956f81bd2b5ac68f7dd91c9d538db16
1di0t <jeonjerry0709@gmail.com>
2025-03-17 11:08:47 +0900

    91501385_v1

--- test.py
+++ test.py
@@ -1,16 +1,8 @@
 import sys
 
-semina = []
-semina_num = int(sys.stdin.readline().rstrip())
-room_num = [["Algorithm","204"],["DataAnalysis","207"],["ArtificialIntelligence","302"],["CyberSecurity","B101"]
-            ,["Network","303"],["Startup","501"],["TestStrategy","105"]]
+integer = list(map(int, sys.stdin.readline().rstrip().split(" ")))
 
-for i in range(semina_num):
-    semina.append(sys.stdin.readline().rstrip())
-
-for i in range(semina_num):
-    for j in range(len(room_num)):
-        if semina[i] == room_num[j][0]:
-            print(room_num[j][1])
-            break
-    
+if integer[0] + integer[1] == integer[2]:
+    print("correct!")
+else:
+    print("wrong!")
