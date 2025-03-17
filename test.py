ee8a4fbaaea764cb0d7e11966b1171fc6ace22d8
1di0t <jeonjerry0709@gmail.com>
2025-03-17 11:38:17 +0900

    91503162_v1

--- test.py
+++ test.py
@@ -1,17 +1,9 @@
 import sys
 
-beers = int(sys.stdin.readline().rstrip())
+ids = list(map(int,sys.stdin.readline().rstrip().split(" ")))
+verify = 0
 
-for i in range(beers,-1,-1):
-    if i == 0:
-        print("No more bottles of beer on the wall, no more bottles of beer.")
-        if beers == 1:
-            print(f"Go to the store and buy some more, {beers} bottle of beer on the wall.")
-        else:
-            print(f"Go to the store and buy some more, {beers} bottles of beer on the wall.")
-    elif i == 1:
-        print("1 bottle of beer on the wall, 1 bottle of beer.")
-        print("Take one down and pass it around, no more bottles of beer on the wall.\n")
-    else:
-        print(f"{i} bottles of beer on the wall, {i} bottles of beer.")
-        print(f"Take one down and pass it around, {i-1} bottles of beer on the wall.\n")
+for i in range(len(ids)):
+    verify += ids[i] ** 2
+
+print(verify % 10)
