13ef9a82f99c1f6346b07175a4fb1f266d21af3a
1di0t <jeonjerry0709@gmail.com>
2025-03-18 21:50:37 +0900

    1152_v1

--- test.py
+++ test.py
@@ -1,16 +1,4 @@
-test_num = int(input())
-case = []*test_num
 
-for i in range(test_num):
-    case.append(list(map(int,input().split(" "))))
+string_arr = input().strip().split(" ")
 
-for i in range(test_num):
-    w_num = case[i][2] // case[i][0]
-    if case[i][2] % case[i][0] != 0:
-        w_num += 1
-    h_num = case[i][2] % case[i][0]
-    if h_num == 0:
-        h_num = case[i][0]
-    if w_num < 10:
-        w_num = '0'+str(w_num)
-    print(str(h_num)+str(w_num))
+print(len(string_arr))
