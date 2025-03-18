4aaba590025135e602ced5bc894e9bd379d10b45
1di0t <jeonjerry0709@gmail.com>
2025-03-18 23:12:09 +0900

    5597_v1

--- test.py
+++ test.py
@@ -1,6 +1,22 @@
-def factorial_recursive(num):
-    if (num <= 1): return 1
-    return num * factorial_recursive(num-1)
+import sys
 
-num = int(input())
-print(factorial_recursive(num))
+id_arr = []
+no = []
+for i in range(28):
+    id_arr.append(int(sys.stdin.readline().rstrip()))
+print("here1")
+id_arr.sort()
+print("here2")
+id,comp_id = 0,1
+while True:
+    if id_arr[id] != comp_id:
+        no.append(comp_id)
+    else:
+        id+=1
+    comp_id+=1
+    if id > 26:
+        break
+    
+
+print(min(no))
+print(max(no))
