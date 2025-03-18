a940242ec2ab64fe701d89fc3e1a7c03a7331abd
1di0t <jeonjerry0709@gmail.com>
2025-03-18 21:37:34 +0900

    10250_v1

--- test.py
+++ test.py
@@ -1,27 +1,13 @@
-import sys
+test_num = int(input())
+case = []*test_num
+H,W =1,1
 
-pay, money1, money2 = map(int, sys.stdin.readline().rstrip().split(" "))
-answer, pay_money, n, m, buff = 0,0,0,0,None
+for i in range(test_num):
+    case.append(list(map(int,input().split(" "))))
 
-def compareChange(pay, total):
-    return True if pay >= total else False
-
-while compareChange(pay, pay_money):
-    while compareChange(pay, pay_money):
-        pay_money = money1 * n + money2 * m
-        if pay_money < pay:
-            m+=1
-            continue
-        elif pay_money == pay:
-            answer = pay_money
-            break
-        else:
-            if buff == None: buff = pay_money
-            elif buff>pay_money: buff = pay_money
-            answer = buff
-    m=0
-    n+=1
-    pay_money = money1 * n + money2 * m
-    
-
-print(answer)
+for i in range(test_num):
+    w_num = case[i][2] // case[i][0] +1
+    h_num = case[i][2] % case[i][0]
+    if w_num < 10:
+        w_num = '0'+str(w_num)
+    print(str(h_num)+str(w_num))
