980d15f3e931f5fc28d6c3675861e75ebdfe5774
1di0t <jeonjerry0709@gmail.com>
2025-03-21 22:54:37 +0900

    1092_v1

--- test.py
+++ test.py
@@ -1,30 +1,19 @@
 import sys
 
-number = int(sys.stdin.readline().rstrip())
-num_arr = list(map(int, sys.stdin.readline().rstrip().split(" ")))
-n1,n2,n3, answer = 0, 0, 0, 0
+crain = int(sys.stdin.readline().rstrip())
+capacity = sorted(list(map(int, sys.stdin.readline().rstrip().split(" "))),reverse=True)
+box_count = int(sys.stdin.readline().rstrip())
+box_weight = sorted(list(map(int, sys.stdin.readline().rstrip().split(" "))),reverse=True)
 
-if number == 0:
-    answer = 0
-elif number == 1:
-    answer = sum(num_arr)-max(num_arr)
+if max(capacity) < max(box_weight):
+    print(-1)
 else:
-    #면이 1개일때 최소값
-    n1 = min(num_arr)
-
-    #면이 2개일때 최소값
-    n2 = min([num_arr[0]+num_arr[1],num_arr[0]+num_arr[2],num_arr[0]+num_arr[3],num_arr[0]+num_arr[4],
-           num_arr[5]+num_arr[1],num_arr[5]+num_arr[2],num_arr[5]+num_arr[3],num_arr[5]+num_arr[4],
-           num_arr[1]+num_arr[2],num_arr[1]+num_arr[3],
-           num_arr[4]+num_arr[2],num_arr[4]+num_arr[3]])
-
-    #면이 3개일때 최소값
-    n3 = min([num_arr[0]+num_arr[1]+num_arr[2],num_arr[0]+num_arr[1]+num_arr[3],num_arr[0]+num_arr[4]+num_arr[2],num_arr[0]+num_arr[4]+num_arr[3],
-              num_arr[5]+num_arr[1]+num_arr[2],num_arr[5]+num_arr[1]+num_arr[3],num_arr[5]+num_arr[4]+num_arr[2],num_arr[5]+num_arr[4]+num_arr[3]])
-    
-    n3_count = 4
-    n2_count = 8*number-12
-    n1_count = 5*(number**2)-(16*number)+12
-
-    answer = n3*n3_count + n2*n2_count+n1*n1_count
-print(answer)
+    result = 0
+    while box_weight:
+        for i in range(crain):
+            for j in range(len(box_weight)):
+                if capacity[i] >= box_weight[j]:
+                    del box_weight[j]
+                    break
+        result += 1
+    print(result)
