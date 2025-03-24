7bd2cfe6547d4b5f42f14f8a06698533fb7742d4
1di0t <jeonjerry0709@gmail.com>
2025-03-24 12:18:49 +0900

    1931_v1

--- test.py
+++ test.py
@@ -1,12 +1,14 @@
 import sys
 
 N = int(sys.stdin.readline().rstrip())
-end_time, meeting = 0,0
+end_time, meeting , meeting_arr= 0,0,[]
 for i in range(N):
     start , end = map(int,sys.stdin.readline().rstrip().split(" "))
-    if start > end_time:
-        end_time = end
+    meeting_arr.append([start,end])
+meeting_arr = sorted(meeting_arr,key=lambda x:(x[1],x[0]))
+for i in meeting_arr:
+    if i[0] >= end_time:
         meeting += 1
-    else:
-        continue
+        end_time = i[1]
+    
 print(meeting)
