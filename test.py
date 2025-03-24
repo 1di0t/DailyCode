d1991f0e2828327a619bd3b1ed1c81758bd91ea8
1di0t <jeonjerry0709@gmail.com>
2025-03-24 10:26:42 +0900

    1074_v1

--- test.py
+++ test.py
@@ -1,39 +1,19 @@
 import sys
 
-def is_same_stack(stack1, stack2, stack2_index):
-    if stack1[-1] == stack2[stack2_index]:
-        return True
+N,r,c = map(int,sys.stdin.readline().rstrip().split(" "))
 
+def z(N,r,c):
+    if N == 0:
+        return 0
+    half = 2**(N-1)
+    if r < half and c < half:
+        return z(N-1,r,c)
+    elif r < half and c >= half:
+        return half*half + z(N-1,r,c-half)
+    elif r >= half and c < half:
+        return 2*half*half + z(N-1,r-half,c)
+    else:
+        return 3*half*half + z(N-1,r-half,c-half)
 
-
-number = int(sys.stdin.readline().rstrip())
-input_stack, output_stack, answer_list = [], [], []
-
-for n in range(number):
-    answer_list.append(int(sys.stdin.readline().rstrip()))
-minus = False
-y=0
-try:
-    for x in range(1,number+1):
-        input_stack.append(x)
-        output_stack.append('+')
-        if is_same_stack(input_stack, answer_list, y):
-            minus = True
-        while minus:
-            if len(input_stack) == 0:
-                break
-            if input_stack[-1] > answer_list[y]:
-                raise Exception
-            if is_same_stack(input_stack, answer_list, y):
-                input_stack.pop()
-                output_stack.append('-')
-                y+=1
-            else:
-                minus = False
-                break
-    for z in output_stack:
-        print(z)
-except:
-        print('NO')
-    
+print(z(N,r,c))
 
