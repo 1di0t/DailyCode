cf149e8832ab8922ad26bf3eb2eaea1e78b3ce98
1di0t <jeonjerry0709@gmail.com>
2025-03-24 12:09:45 +0900

    1931_v1

--- test.py
+++ test.py
@@ -1,19 +1,12 @@
 import sys
 
-N,r,c = map(int,sys.stdin.readline().rstrip().split(" "))
-
-def z(N,r,c):
-    if N == 0:
-        return 0
-    half = 2**(N-1)
-    if r < half and c < half:
-        return z(N-1,r,c)
-    elif r < half and c >= half:
-        return half*half + z(N-1,r,c-half)
-    elif r >= half and c < half:
-        return 2*half*half + z(N-1,r-half,c)
+N = int(sys.stdin.readline().rstrip())
+end_time, meeting = 0,0
+for i in range(N):
+    start , end = map(int,sys.stdin.readline().rstrip().split(" "))
+    if start > end_time:
+        end_time = end
+        meeting += 1
     else:
-        return 3*half*half + z(N-1,r-half,c-half)
-
-print(z(N,r,c))
-
+        continue
+print(meeting)
