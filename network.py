699b584d2e98ff5296bcd2fbcd30fe89f66fbdc8
1di0t <jeonjerry0709@gmail.com>
2025-05-10 21:55:11 +0900

    PRogrammers airport #1

--- network.py
+++ network.py
@@ -1,19 +1,29 @@
+from collections import Counter
 
-
-def solution(n, computers):
-    answer = 0
-    visited = [False] * n
-    def dfs(node):
-        visited[node] = True
-        for i in range(n):
-            if computers[node][i] == 1 and not visited[i]:
-                dfs(i)
-            
-    for i in range(n):
-        if not visited[i]:
-            dfs(i)
-            answer += 1
-
-    return answer
-
-print(solution(3, [[1, 1, 0], [1, 1, 0], [0, 0, 1]])) 
+def solution(tickets):
+    answer = []
+    tickets.sort(key=lambda x: (x[0], x[1]))
+    tickets_dict = {}
+    for ticket in tickets:
+        if ticket[0] not in tickets_dict:
+            tickets_dict[ticket[0]] = []
+        tickets_dict[ticket[0]].append(ticket[1])
+    
+    def dfs(start, path):
+        if len(path) == len(tickets) + 1:
+            return path
+        if start not in tickets_dict:
+            return None
+        for i in range(len(tickets_dict[start])):
+            next_city = tickets_dict[start][i]
+            tickets_dict[start].pop(i)
+            path.append(next_city)
+            result = dfs(next_city, path)
+            if result:
+                return result
+            path.pop()
+            tickets_dict[start].insert(i, next_city)
+        return None
+    
+    return dfs("ICN", ["ICN"])
+print(solution([["ICN", "SFO"], ["ICN", "ATL"], ["SFO", "ATL"], ["ATL", "ICN"], ["ATL","SFO"]]))
